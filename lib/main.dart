6ed45589f8e8ac781fc10bcb5de45a512eb9ab3b
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-09 14:49:42 +0100

    initialisation du projet

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Quiz App',
+      home: Scaffold(
+        body: Center(
+          child: Text('Quiz App!'),
+        ),
+      ),
+    ),
+  );
+}

d6f47c1db26d0224851ada52ecdb1cd78276f063
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-11 16:14:50 +0100

    Configuration d'une colonne

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -10,6 +10,18 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Question screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+        ],
+      ),
+    );
   }
 }

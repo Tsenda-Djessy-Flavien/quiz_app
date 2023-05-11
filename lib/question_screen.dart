dd8a03f08b28daad6056e03cfc9f58cd2c0c1d88
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-11 19:07:35 +0100

    Alignment, Margin & Padding

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -15,25 +15,32 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
-          // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),
-          // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[2]),
-          // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[3]),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          // mainAxisAlignment -> centré par rapport a la vertical
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment -> centré par rapport a l'horizontal
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
+            // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),
+            // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[2]),
+            // AnswerButton(onTap: () {}, answerText: currentQuestion.answers[3]),
 
-          // convertir la liste String des reponse de QuizQuestion en liste de Widget button des reponses
-          ...currentQuestion.answers.map((answer) {
-            // ... extrait la liste enfant dans la liste parent // e.g :  strat -> [[1, 2, 3], 4], end -> [1, 2, 3, 4]
-            return AnswerButton(onTap: () {}, answerText: answer);
-          }),
-        ],
+            // convertir la liste String des reponse de QuizQuestion en liste de Widget button des reponses
+            ...currentQuestion.answers.map((answer) {
+              // ... extrait la liste enfant dans la liste parent // e.g :  strat -> [[1, 2, 3], 4], end -> [1, 2, 3, 4]
+              return AnswerButton(onTap: () {}, answerText: answer);
+            }),
+          ],
+        ),
       ),
     );
   }

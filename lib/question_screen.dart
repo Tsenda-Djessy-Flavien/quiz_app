577460ed12cb9efda720f653187180bdfaeaba00
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-11 17:28:57 +0100

    Accéder aux éléments de liste et aux propriétés d'objet

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/Answer_button.dart';
+import 'package:quiz_app/data/question.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -9,6 +10,7 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final currentQuestion = questions[0];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -16,14 +18,15 @@ class _QuestionScreenState extends State<QuestionScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(onTap: () {}, answerText: 'Answer 1'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 2'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 3'),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[2]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[3]),
         ],
       ),
     );
